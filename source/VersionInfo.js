08df620e75053b9c6614e0ea56e91973af7e946c
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-13 19:10:23 +1000

    Create VersionInfo.js

---
+++ source/VersionInfo.js
@@ -0,0 +1,11 @@
+
+"use strict";
+const Git = require("trainmppe/utils/Git");
+const VersionString = require("trainmppe/utils/VersionString");
+
+class VersionInfo {
+    static NAME = "TrainMPPE";
+    static BASE_VERSION = "10.1";
+    static IS_DEVELOPMENT_BUILD = true;
+    static BUILD_CHANNEL = "stable";
+}
